e7bfd294de38dd053cb898977f876a41ed5ec95b
Nayggets <quentin.melotte@etu.umontpellier.fr>
2022-12-01 22:55:43 +0100

    ClientHandler

--- src/main/java/fr/osmium/meregrand/ClientHandler.java
+++ src/main/java/fr/osmium/meregrand/ClientHandler.java
@@ -1,5 +1,9 @@
 package fr.osmium.meregrand;
 
+import com.sun.jdi.OpaqueFrameException;
+import fr.osmium.meregrand.Paquet.AUTHPACKET;
+import fr.osmium.meregrand.Paquet.Packet;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -20,9 +24,24 @@ public class ClientHandler extends Thread {
     @Override
     public void run() {
         try {
+
             ServiceManager.LOGGER.info("Client connected");
+            Packet packet = (Packet) in.readObject();
+            switch (packet.getType()){
+                case AUTHPACKET -> {
+                    AUTHPACKET packetAuth = (AUTHPACKET) packet;
+                    String[] str = packetAuth.toString().split(",");
+                    String mail = str[0];
+                    String mdp = str[1];
+                    //if(verifPassword()){
+                    // Creation du jeton et envois du jeton via JetonPacket
+                    //}
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
